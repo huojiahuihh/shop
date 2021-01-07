e1218ec49da5a77bb86cdebecbe0cf329108f154
huojiahuihh <huojiahuihh@163.com>
2021-01-07 22:07:45 +0800

    商品列表新增 修改

---
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/mapper/SpuDetailMapper.java
@@ -0,0 +1,9 @@
+package com.baidu.shop.mapper;
+
+
+import com.baidu.shop.entity.SpuDetailEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SpuDetailMapper extends Mapper<SpuDetailEntity> {
+
+}
